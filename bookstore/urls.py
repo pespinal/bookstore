9ed10396f2b3ff90f2b646bacc296f21033124af
Paolangela Espinal <pao93esp@gmail.com>
2015-10-13 13:55:01 -0700

    Add store app, view, template and URL

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'store.views.index', name='index'),
+    url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
