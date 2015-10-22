6f823d5d4f24172336340d1b191d7ce15128f9ce
Paolangela Espinal <pao93esp@gmail.com>
2015-10-22 11:13:42 -0700

    Added shopping cart and book orders template, views and model. You can add and remove books from the cart

--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Book
+from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import Book, BookOrder, Cart
+
 
 # Create your views here.
 def index(request):
@@ -11,9 +15,6 @@ def store(request):
         'books': books,
         'page': 'Bookstore',
     }
-    request.session['location'] = "unknown"
-    if request.user.is_authenticated():
-        request.session['location'] = "earth"
     return render(request, 'base.html', context)
 
 def book_details(request, book_id):
@@ -21,3 +22,54 @@ def book_details(request, book_id):
         'book': Book.objects.get(pk=book_id),
     }
     return render(request, 'store/detail.html', context)
+
+def add_to_cart(request, book_id):
+    if request.user.is_authenticated():
+        try:
+            book = Book.objects.get(pk=book_id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            try:
+                cart = Cart.objects.get(user=request.user, active = True)
+            except ObjectDoesNotExist:
+                cart = Cart.objects.create(
+                    user=request.user
+                )
+                cart.save()
+            cart.add_to_cart(book_id)
+        return redirect('cart')
+    else:
+        return redirect('index')
+
+def remove_from_cart(request, book_id):
+    if request.user.is_authenticated():
+        try:
+            book = Book.objects.get(pk=book_id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            cart = Cart.objects.get(user=request.user.id, active=True)
+            cart.remove_from_cart(book_id)
+        return redirect('cart')
+    else:
+        return redirect('index')
+
+def cart(request):
+    if request.user.is_authenticated():
+        cart = Cart.objects.filter(user=request.user.id, active= True)
+        orders = BookOrder.objects.filter(cart=cart)
+        total = 0
+        count = 0
+        for order in orders:
+            total += (order.book.price * order.quantity)
+            count += order.quantity
+        context = {
+            'cart': orders,
+            'total': total,
+            'count': count,
+        }
+        return render(request, 'store/cart.html', context)
+    else:
+        return redirect('index')
+
